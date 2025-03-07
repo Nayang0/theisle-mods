d6c28c564f953099d5ee32cff296150a60147ea8
Nayang0 <tu-email@ejemplo.com>
2025-03-06 21:40:18 -0600

    Add all project files: - Main launcher code - Configuration files - Build specifications - Documentation - Dependencies list

---
+++ launcher_spec.py
@@ -0,0 +1,45 @@
+block_cipher = None
+
+a = Analysis(
+    ['import tkinter as tk.py'],
+    pathex=['c:\\Users\\nayang\\Desktop\\TheIsle2.0'],
+    binaries=[],
+    datas=[
+        ('config.json', '.'),
+        ('creators.json', '.'),
+        ('servers.json', '.')
+    ],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
+    noarchive=False,
+)
+
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    [],
+    name='TheIsle Legacy Launcher',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    icon='icon.ico'  # Agrega un ícono si lo tienes
+)
